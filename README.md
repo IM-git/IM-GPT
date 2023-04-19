7d47d8830c92ba157b9c06fb1d05d6a8867fe2ff
IM94 <gladiator.pw@gmail.com>
2023-04-19 23:41:34 +0300

    Created a main() function.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Glad-GPT
+
+## My Goals:
+1. Need to save the chat history.
+2. 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import openai
+import json
+import os
 
 from settings import TOKEN_GPT
 
@@ -6,14 +8,19 @@ openai.api_key = TOKEN_GPT
 messages = []
 
 
+def main(message: str):
+    messages.append({"role": "user", "content": message})
+    chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+    reply = chat.choices[0].message.content
+    print(f"ChatGPT: {reply}")
+    messages.append({"role": "assistant", "content": reply})
+
+
 if __name__ == '__main__':
+
+    # Run GPT BOT
     while True:
         message = input("User: ")
         if message == "q":
             break
-
-        messages.append({"role": "user", "content": message})
-        chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-        reply = chat.choices[0].message.content
-        print(f"ChatGPT: {reply}")
-        messages.append({"role": "assistant", "content": reply})
+        main(message)
