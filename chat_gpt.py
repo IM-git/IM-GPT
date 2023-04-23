ac5d32eeaa287c0d2dea8c7ff2d5d5bd01d250e9
IM94 <gladiator.pw@gmail.com>
2023-04-23 23:02:10 +0300

    Added ability to convert text into a voice message

--- chat_gpt.py
+++ chat_gpt.py
@@ -1,11 +1,14 @@
 import openai
 import json
 import time
+from gtts import gTTS
+from playsound import playsound
+import pyttsx3
 import os
 
 from settings import TOKEN_GPT
 
-# API key
+# API token
 openai.api_key = TOKEN_GPT
 
 # The file where the conversation history is stored
@@ -19,6 +22,8 @@ def run_chat_gpt(query: str):
     messages = [{"role": "user", "content": query}]
     reply = get_response_from_gpt(messages)
     print(f"ChatGPT: {reply}")
+    google_voice_acting(reply)
+    # pyttsx3_voice_acting(reply)
     messages.append({"role": "assistant", "content": reply})
     # save_conversation_in_json(data_story_conversation_json, messages)
     save_conversation_in_jsonl(data_story_conversation_jsonl, messages)
@@ -48,6 +53,36 @@ def save_conversation_in_jsonl(file_path: str, messages: list) -> None:
     with open(file_path, "a") as file_name:
         for line in messages:
             json.dump(line, file_name)
+
+
+def google_voice_acting(text: str):
+    """Uses gtts library to interface with Google Translates text to speech API.
+    It requires an Internet connection. It's pretty easy to use."""
+    mp3_file = "mp3_file.mp3"
+    if os.path.exists(mp3_file):
+        os.remove(mp3_file)
+    tts = gTTS(text, lang='en')
+    tts.save(mp3_file)
+    try:
+        playsound(mp3_file)
+    except BaseException():
+        print("Something wrong. Your ad could be here...")
+
+
+def pyttsx3_voice_acting(text: str):
+    """Uses pyttsx3 library to interface text to speech API.
+    Offline text to speech."""
+    mp3_file = "mp3_file.mp3"
+    if os.path.exists(mp3_file):
+        os.remove(mp3_file)
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 180)    # setting up new voice rate
+    engine.setProperty('volume', 1.0)   # setting up volume level  between 0 and 1
+    voices = engine.getProperty('voices')  # getting details of current voice
+    engine.setProperty('voice', voices[1].id)   # changing index, changes voices. 1 for en, 0 for ru.
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
 
 
 if __name__ == '__main__':
