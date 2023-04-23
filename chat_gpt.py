bfd8b2cc8594dbf1ed91b768c8cc33d7f73cd08f
IM94 <gladiator.pw@gmail.com>
2023-04-23 19:35:30 +0300

    Added the ability to save the message history in jsonl format for the chat_gpt file.

--- chat_gpt.py
+++ chat_gpt.py
@@ -9,7 +9,8 @@ from settings import TOKEN_GPT
 openai.api_key = TOKEN_GPT
 
 # The file where the conversation history is stored
-data_story_conversation = "chat_history.json"
+data_story_conversation_json = "chat_history.jsonl"
+data_story_conversation_jsonl = "chat_history.jsonl"
 
 
 def run_chat_gpt(query: str):
@@ -19,7 +20,8 @@ def run_chat_gpt(query: str):
     reply = get_response_from_gpt(messages)
     print(f"ChatGPT: {reply}")
     messages.append({"role": "assistant", "content": reply})
-    save_conversation(data_story_conversation, messages)
+    # save_conversation_in_json(data_story_conversation_json, messages)
+    save_conversation_in_jsonl(data_story_conversation_jsonl, messages)
 
 
 def get_response_from_gpt(messages: list) -> list:
@@ -33,12 +35,19 @@ def read_file(file_path: str) -> list | dict | tuple:
     return data
 
 
-def save_conversation(file_path: str, messages: list) -> None:
+def save_conversation_in_json(file_path: str, messages: list) -> None:
     """Saving the message to a JSON file"""
     data = read_file(file_path)
     data[time.strftime('%Y-%m-%dT%H:%M:%S')] = messages
     with open(file_path, "w") as file_name:
         json.dump(data, file_name)
+
+
+def save_conversation_in_jsonl(file_path: str, messages: list) -> None:
+    """Saving the message to a JSONL file"""
+    with open(file_path, "a") as file_name:
+        for line in messages:
+            json.dump(line, file_name)
 
 
 if __name__ == '__main__':
